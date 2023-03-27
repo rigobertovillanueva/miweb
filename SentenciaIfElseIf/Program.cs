8141e34685d013eaabe3194bd67deb16723a6e6c
rigobertovillanueva <rigo.villanueva@gmail.com>
2023-03-27 16:25:22 -0300

    commit carpeta visual studio

---
+++ SentenciaIfElseIf/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        bool areYouHungry = true;
+        bool youHaveMoney = true;
+
+        if (areYouHungry && youHaveMoney && IsOpenRestaurant("pepe", 9))
+        {
+            Console.WriteLine("eat something");
+
+        }
+
+        else
+        {
+            Console.WriteLine("dont eat something");
+        }
+
+
+
+        static bool IsOpenRestaurant(string name, int hour = 0)
+
+        {
+
+            if (name == "pepe" && hour > 8 && hour < 23)
+            { return true; }
+            else if (name == "Restaurant 24 horas")
+            { return true; }
+            else
+            { return false; }
+
+        }
+
+
+
+
+    }
+
+
+
+ 
+
+
+}
+
